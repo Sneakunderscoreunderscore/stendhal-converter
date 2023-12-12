e902f191cf8cf6980e1c40f91b82e7be2bf94694
Sneakunderscoreunderscore <rhionryan@gmail.com>
2023-12-12 16:14:01 +0000

    fixed minor issues with conversion

--- converter.py
+++ converter.py
@@ -1,4 +1,3 @@
-
 # functions
 
 def writepage(page):
@@ -6,23 +5,20 @@ def writepage(page):
     book.write("\n#- "+page)
     book.close
 
-
 # take info (title, author)
 # title is the book title
 # author is the book author
 # copy is the text
 # file thats going to be copied
 title = input("title :") 
-author = input("author :")
+author = "Sneak__ on discord"
 copy = input("file you want to copy: ")
 filename = title+".stendhal"
-
 
 # created the output file with starting info
 book = open(filename,"w")
 book.write("title: "+title+"\nauthor: "+author+"\npages:")
 book.close()
-
 
 # reading the copy text
 copytext = open(copy,"r")
@@ -35,24 +31,22 @@ pagetext = ""
 lettercount = 0
 linecount = 0
 
-
-for i in range(0,len(text)-1):
+for i in range(0,len(text)-1): # for every word
     wordlength = len(text[i])
-
     if lettercount+wordlength >19: # if the current line is 'full' (based of the normal letter width)
         pagetext += "\n" # move to the next line
         linecount += 1
         lettercount = 0
-
     if linecount == 10: # if the page is 'full' (arbitarily set to 10 lines)
         writepage(pagetext)
         linecount = 0
         pagetext = ""
-
+        lettercount = 0
     if text[i] == "⌬": # if the character is a line break
         pagetext += "\n"
         linecount += 1
+        lettercount = 0
     else:
-        pagetext += text[i] + " "
-        lettercount += len(text[i])
+        pagetext += text[i] + " " # else add the word
+        lettercount += len(text[i])+1
 
